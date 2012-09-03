4f6eb9e3b2371e4d1cce2c865955f30dbee4af66
Robert Kowalski <rok@kowalski.gd>
2012-09-03 23:04:38 +0200

    Enyo Tutorial: Part 1

--- source/App.js
+++ source/App.js
@@ -1,10 +1,28 @@
 enyo.kind({
 	name: "App",
-	fit: true,
-	components:[
-		{name: "hello", content: "Hello World", allowHtml: true, ontap: "helloWorldTap"}
+	kind: enyo.Control,
+	style: "",
+	classes: "onyx",
+	components: [
+		{kind: "onyx.InputDecorator", components: [
+			{kind: "onyx.Input", name: "sumControl", placeholder: "Enter sum"}
+		]},
+		{kind: "onyx.InputDecorator", components: [
+			{kind: "onyx.Input", name: "percentControl", placeholder: "Enter percent"}
+		]},
+		{kind: "onyx.Button", content: "Calculate tip", ontap: "calculate"},
+		{tag: "div", name: "tipAmount"}
 	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.hello.addContent("<br/><b>hello</b> control was tapped");
+	create: function() {
+		this.inherited(arguments);
+	},
+	calculate: function(inSource, inEvent) {
+		var sum = this.$.sumControl.hasNode().value;
+		var percent = this.$.percentControl.hasNode().value;
+
+		var result = (sum * percent) / 100;
+		this.$.tipAmount.setContent(result);
+
+		return true; // stop bubbling
 	}
 });
